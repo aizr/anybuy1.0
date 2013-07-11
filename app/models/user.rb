896de542baa71b3a9c8855de6bda196eca0af3e0
aizr <aizr1212@gmail.com>
2013-07-11 16:31:27 +0800

    修正驗證使用者字數

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,21 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :username, :cname, :phone, :birthday, :nid, :address, :terms_of_service, :name, :uid, :provider
+  attr_accessible :email, :password, :password_confirmation, :username, :cname, :phone, :birthday, :nid, :address, :terms_of_service, :name, :uid, :provider, :money
   validates_presence_of :email, :password, :password_confirmation, :username, :cname, :phone, :birthday, :nid, :address, :message => "不能空白喔"
 
   #validates_confirmation_of :password
   validates_acceptance_of :terms_of_service, :message => "請勾選"
   validates :username, :cname, :presence => true,
-      :length => {:minimum => 4, :maximum => 20, :message => "長度不正確"},
+      :length => {:minimum => 3, :maximum => 20, :message => "長度不正確"},
       :uniqueness => { :message => "姓名重複" }
 
   validates :password, :password_confirmation, :presence => true,
       :length => {:minimum => 6, :message => "最少6個字"}
-      
-  validates :phone, :nid, :presence => true, 
+
+  validates :phone, :nid, :presence => true,
       :length => {:minimum => 10, :message => "最少10個字"}
-      
+
   validates_numericality_of :phone, :only_integer => true , :message => "請輸入數字"
   validates_format_of :email, :with => /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/, :message => "格式錯誤"
   validates_format_of :phone, :with => /^[0-9]{4}[0-9]{6}$/, :message => "格式錯誤"
